cda65f57cd3b2cb2620e65fc8f15db2a0a75d6e9
anietomoliner <121170490+anietomoliner@users.noreply.github.com>
2022-12-24 18:12:16 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,13 @@ pins.digitalWritePin(DigitalPin.P8, 1)
 basic.showNumber(Distancia)
 basic.forever(function () {
     Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
-    basic.showNumber(Distancia)
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        pins.digitalWritePin(DigitalPin.P12, 1)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
-        basic.showNumber(Distancia)
-    }
     if (Distancia > 5) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
+            basic.showNumber(Distancia)
+        }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             pins.digitalWritePin(DigitalPin.P8, 1)
             pins.digitalWritePin(DigitalPin.P12, 1)
