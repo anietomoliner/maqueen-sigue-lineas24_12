91df530d75f9fc076b2448fa1805abcddab09329
anietomoliner <121170490+anietomoliner@users.noreply.github.com>
2022-12-24 18:46:07 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,32 @@
+function Gira_D (avanzar_: boolean) {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 154)
+}
 function Avanza (avanzar_: boolean) {
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P12, 1)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
-    basic.showNumber(Distancia)
 }
 function Retroceder (avanzar_: boolean) {
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P12, 1)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 154)
-    basic.showNumber(Distancia)
 }
 function Gira_I (avanzar_: boolean) {
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P12, 0)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 154)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-    basic.showNumber(Distancia)
 }
-let Distancia = 0
-Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
+let Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
 pins.digitalWritePin(DigitalPin.P12, 1)
 pins.digitalWritePin(DigitalPin.P8, 1)
-basic.showNumber(Distancia)
 basic.forever(function () {
     Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
     if (Distancia > 5) {
+        basic.showNumber(Distancia)
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             Retroceder(true)
         }
@@ -35,11 +37,7 @@ basic.forever(function () {
             Gira_I(true)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P12, 0)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 154)
-            basic.showNumber(Distancia)
+            Gira_D(true)
         }
     } else {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
