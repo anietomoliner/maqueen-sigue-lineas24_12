deffc28911e4703cc66329bc34eb374f97525308
anietomoliner <121170490+anietomoliner@users.noreply.github.com>
2022-12-24 18:37:17 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
-let Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
+function Avanza (avanzar_: boolean) {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
+    basic.showNumber(Distancia)
+}
+function Retroceder (avanzar_: boolean) {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 154)
+    basic.showNumber(Distancia)
+}
+function Gira_I (avanzar_: boolean) {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 154)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    basic.showNumber(Distancia)
+}
+let Distancia = 0
+Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
 pins.digitalWritePin(DigitalPin.P12, 1)
 pins.digitalWritePin(DigitalPin.P8, 1)
 basic.showNumber(Distancia)
@@ -6,23 +26,13 @@ basic.forever(function () {
     Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
     if (Distancia > 5) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P12, 1)
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
-            basic.showNumber(Distancia)
+            Retroceder(true)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P12, 1)
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
-            basic.showNumber(Distancia)
+            Avanza(true)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P12, 0)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 154)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            basic.showNumber(Distancia)
+            Gira_I(true)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             pins.digitalWritePin(DigitalPin.P8, 1)
